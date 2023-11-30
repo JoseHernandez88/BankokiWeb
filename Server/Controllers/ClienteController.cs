d4d4c736e904b2ba817895c5d02700a10714f628
JoseHernandez88 <47012294+JoseHernandez88@users.noreply.github.com>
2023-11-30 00:32:44 -0400

    Cliente Controler methods up. -GetAllClientes -GetClienteByEmail {email} -CetClienteVerifyPassword (password, email} -Loggedin {email} -LoggedOut {email}

--- Server/Controllers/ClienteController.cs
+++ Server/Controllers/ClienteController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BakokiWeb.Server.Data;
+using BakokiWeb.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BakokiWeb.Server.Controllers
 {
@@ -7,5 +10,97 @@ namespace BakokiWeb.Server.Controllers
 	[ApiController]
 	public class ClienteController : ControllerBase
 	{
+		private readonly DataContext _context;
+
+		public ClienteController(DataContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<Cliente>>> GetAllClient()
+		{
+			try
+			{
+				var list = await _context.Clientes.ToListAsync();
+				return Ok(list);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+		[HttpGet("{email}")]
+		public async Task<ActionResult<Cliente>> GetClienteByEmail(string email)
+		{
+			try
+			{
+				var cli = await _context.Clientes.FindAsync(email);
+				return Ok(cli);
+			} catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+		[HttpGet("{password},{email}")]
+		public async Task<ActionResult<Cliente>> GetClienteVerifyPassword(string password, string email)
+		{
+			try
+			{
+				var cli = await GetClienteByEmail(email);
+				bool result = false;
+				if (cli.Value != null)
+				{
+					result = cli.Value.Password.Equals(password);
+					if (result)
+					{
+						return Ok(cli.Value);
+					}
+					else
+					{
+						return NotFound("Username or password do not match.");
+					}
+
+				}
+				else
+				{
+					throw new Exception($"Cliente Control: Could not find {email}");
+				}
+			} catch (Exception ex)
+			{
+				return BadRequest(ex.ToString());
+			}
+		}
+		[HttpPut("{email}")]
+		public async Task<ActionResult<Cliente>> LoggedIn(string email)
+		{
+			var cli = await GetClienteByEmail(email);
+			if (cli.Value != null)
+			{
+				cli.Value.LoggedIn = true;
+				await _context.SaveChangesAsync();
+				return Ok(cli);
+			}
+			else
+			{
+				return BadRequest($"Cliente Controller:Could not find {email}");
+			}
+		}
+		[HttpPut("{email}")]
+		public async Task<ActionResult<Cliente>> Loggedoff(string email)
+		{
+			var cli = await GetClienteByEmail(email);
+			if (cli.Value != null)
+			{
+				cli.Value.LoggedIn = false;
+				await _context.SaveChangesAsync();
+				return Ok(cli);
+			}
+			else
+			{
+				return BadRequest($"Cliente Controller:Could not find {email}");
+			}
+		}
+
 	}
 }
